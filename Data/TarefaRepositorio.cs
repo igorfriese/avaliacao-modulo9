005b85e1a30ad8c99b181554f432765a7ab49d37
Gabzao <gabrielbell2002felipe@gmail.com>
2026-07-31 00:53:20 -0300

    criando tarefas para o crud

--- Data/TarefaRepositorio.cs
+++ Data/TarefaRepositorio.cs
@@ -1,6 +1,57 @@
-﻿namespace avaliacao_modulo9.Data
+﻿using avaliacao_modulo9.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace avaliacao_modulo9.Data
 {
-    public class TarefaRepositorio
+    public class TarefaRepositorio : ITarefaRepositorio
     {
+        private readonly AppDbContext _context;
+
+        public TarefaRepositorio(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Tarefa>> ObterTodasAsync()
+        {
+            return await _context.Tarefas.ToListAsync();
+        }
+
+        public async Task<Tarefa> ObterPorIdAsync(int id)
+        {
+            return await _context.Tarefas.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<Tarefa> CriarAsync(Tarefa tarefa)
+        {
+            _context.Tarefas.Add(tarefa);
+
+            await _context.SaveChangesAsync();
+
+            return tarefa;
+        }
+      
+        public async Task<Tarefa> AtualizarAsync(Tarefa tarefa)
+        {
+            _context.Tarefas.Update(tarefa);
+
+            await _context.SaveChangesAsync();
+
+            return tarefa;
+        }
+
+        public async Task<bool> DeletarAsync(int id)
+        {
+            var tarefa = await ObterPorIdAsync(id);
+
+            if (tarefa == null)
+                return false;
+
+            _context.Tarefas.Remove(tarefa);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
